42eb088fd86e16f11e00ab3c2c5921cbdb6b828f
Silveronski <153466946+Silveronski@users.noreply.github.com>
2024-04-23 16:55:56 +0300

    responsive style changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Logo from '../assets/images/Logo.png';
+import hamburger from '../assets/images/hamburger.png';
 
 const Navbar = () => {
   return (
     <header>
         <nav className='main-navbar'>
             <ul>
+                <li><img className='hamburger' src={hamburger} alt="" /></li>
                 <li>צור קשר</li>
                 <li>טיפולים</li>
                 <li>עליי</li>                    

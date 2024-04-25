b02186c66b1a3d9043692a2348bd181e7c890df2
Silveronski <153466946+Silveronski@users.noreply.github.com>
2024-04-25 03:04:30 +0300

    made navbar responsive with animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Logo from '../assets/images/Logo.png';
+import close from '../assets/images/close.png';
 import hamburger from '../assets/images/hamburger.png';
 
 const Navbar = () => {
+
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+  const navbar = useRef();
+  const imgRef = useRef();
+
+  const onAnimationEnd = () => {
+    navbar.current.style.display = 'none';
+  }
+
+  const handleHamburgerClick = () => {
+    setIsNavbarOpen(!isNavbarOpen);
+    if (isNavbarOpen){
+      imgRef.current.src = hamburger;
+      navbar.current.style.animation = "slideInFromDown 0.4s ease";  
+      navbar.current.addEventListener("animationend", onAnimationEnd, { once: true });    
+    }
+    else {
+      navbar.current.style.animation = "slideInFromAbove 0.4s ease";
+      navbar.current.style.display = 'block';
+      setTimeout(() => {
+        imgRef.current.src = close;        
+      }, 150);
+    }
+  }
+
   return (
     <header>
-        <nav className='main-navbar'>
-            <img className='hamburger' src={hamburger} alt="" />
+        <img ref={imgRef} onClick={handleHamburgerClick} className='hamburger' src={hamburger} alt=""/> 
+        <nav ref={navbar} className='main-navbar'>
             <ul>
                 <li>צור קשר</li>
                 <li>טיפולים</li>
                 <li>עליי</li>                    
-                <li><img src={Logo} alt=""/> אנה - לב פתוח לגוף ולנפש</li>              
+                <li className='navbar-logo'><img src={Logo} alt=""/> אנה - לב פתוח לגוף ולנפש</li>              
             </ul>
         </nav>
     </header>

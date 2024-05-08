6dbca3e8c811b35ca9b3fc9dabcda2f177cdab72
Silveronski <153466946+Silveronski@users.noreply.github.com>
2024-05-08 11:25:39 +0300

    קק

--- src/components/CarouselComp.jsx
+++ src/components/CarouselComp.jsx
@@ -1,10 +1,10 @@
 import Carousel from 'react-bootstrap/Carousel';
-import one from '../assets/images/one.jpg';
-import two from '../assets/images/two.jpg';
-import three from '../assets/images/three.jpg';
 import { motion } from 'framer-motion';
 import { carouselImgVariants, carouselInfoVariants } from '../utils/Variants';
 import content from '../site-content-json/content.json';
+import gallery7 from '../assets/images/gallery7.png';
+import gallery8 from '../assets/images/gallery8.png';
+import carouselImg from '../assets/images/carouselImg.jpg';
 
 const CarouselComp = () => {
   return (
@@ -18,7 +18,7 @@ const CarouselComp = () => {
         <h1>{content.general.name}</h1>          
         <p><span>Senior masseuse</span> <i>&</i> <span>Natural Pharmacist</span></p>
         <button         
-         onClick={() => window.open("https://wa.me/123123123", "_blank")}>BOOK A VISIT
+         onClick={() => window.open(`${content.general.whatsapp}`, "_blank")}>BOOK A VISIT
         </button>
       </motion.div>
 
@@ -28,15 +28,15 @@ const CarouselComp = () => {
             variants={carouselImgVariants}
             initial="hidden"
             animate="show" 
-            src={one}/>                     
+            src={gallery8}/>                     
         </Carousel.Item>
 
         <Carousel.Item>
-            <img src={two}/>   
+            <img src={carouselImg}/>   
         </Carousel.Item>
 
         <Carousel.Item>     
-            <img src={three}/>
+            <img src={gallery7}/>
         </Carousel.Item>
       </Carousel>
 
